116d4c2578d10213f1c0d814486741c82e1f8ab0
Ola Oladapo <olaoladapo007@gmail.com>
2022-10-02 11:49:27 +0100

    login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import Movies from "./components/movies";
+import { Switch, Redirect, Route } from "react-router-dom";
+import LoginForm from "./components/loginForm";
+import MovieForm from "./components/movieForm";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
+import React from "react";
 import "./App.css";
 
 function App() {
   return (
-    <main className="container">
-      <Movies />
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/login" component={LoginForm} />
+          <Route path="/movies/:id" component={MovieForm} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
